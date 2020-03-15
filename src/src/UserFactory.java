d2ff22328628d7d0c5d7b57861ef117c78b4c97c
ankusha <ankushaim@hotmail.com>
2020-03-15 14:00:29 +0530

    user details change issue resolved

--- src/src/UserFactory.java
+++ src/src/UserFactory.java
@@ -24,7 +24,28 @@ public class UserFactory {
         view_controller();
     }
 
+    static void printOptions() {
+        System.out.println();
+        System.out.print("1. Edit or Change details: " + "\t");
+        System.out.print("2. View all routes: "+ "\t");
+        System.out.println("3. Show my route : "+ "\t");
+        System.out.print("4. Request to cancel the Bus Pass: " + "\t");
+        System.out.print("5. Request to suspend the pass: " + "\t");
+        System.out.println("6. Request for new route: " + "\t");
+        System.out.print("7. Print your pass: " + "\t");
+        System.out.println("8. go to previous Menu: " + "\t");
+    }
 
+    static void pressAnyKeyToContinue()
+    {
+        System.out.println("Press Enter key to continue...");
+        try
+        {
+            System.in.read();
+        }
+        catch(Exception e)
+        {}
+    }
 
     void view_controller() {
         System.out.println("Welcome " + Name);
@@ -32,29 +53,35 @@ public class UserFactory {
         User calling_user = new User(userId);
 
         System.out.println("Select appropriate activity to perform");
-        System.out.println("1. Edit or Change details: ");
+        /*System.out.println("1. Edit or Change details: ");
         System.out.println("2. View all routes: ");
         System.out.println("3. Show my route : ");
         System.out.println("4. Request to cancel the Bus Pass: ");
         System.out.println("5. Request to suspend the pass: ");
         System.out.println("5. Request for new route: ");
         System.out.println("6. Print your pass: ");
-        System.out.println("7. go to previous Menu: ");
+        System.out.println("7. go to previous Menu: ");*/
+        printOptions();
         boolean c = true;
 
         while (c) {
-            System.out.println("Input: ");
+            System.out.print("Input: ");
             Scanner input = new Scanner(System.in);
             int choice = input.nextInt();
             switch (choice) {
                 case 1:
                     calling_user.updateUserDetails();
+                    printOptions();
                     break;
                 case 2:
                     calling_user.viewRoute();
+                    pressAnyKeyToContinue();
+                    printOptions();
                     break;
                 case 3:
                     calling_user.viewStops();// To be decided..
+                    pressAnyKeyToContinue();
+                    printOptions();
                     break;
                 case 4:
                     break;
