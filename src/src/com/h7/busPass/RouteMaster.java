21c6e93ede34d0411ba6de0d44dfb29d20dbbf0a
ithikkat <priyank.ithikkat@gmail.com>
2020-03-31 00:26:27 +0530

    comments

--- src/src/com/h7/busPass/RouteMaster.java
+++ src/src/com/h7/busPass/RouteMaster.java
@@ -4,17 +4,22 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public abstract class RouteMaster {
-    
-    ArrayList<String> routes = null;
-    SQLSelect sqlRun = new SQLSelect();
+    /*abstract class which caters to Route services. 
+     * This class is extended by AdminRouteMaster, UserRouteMaster, GuestRouteMAster     * 
+     */
+		
+    ArrayList<String> routes = null; 
+    SQLSelect sqlRun = new SQLSelect(); //SQLselect class object declared in class to maintain single connection with the DB
 
+    //method to display all routes from the DB which also returns an ArrayList with all routes. Shared by admin, guest
     ArrayList<String> viewAllRoutes() throws SQLException {
 
-        String sql = "select distinct route from route_info";
-        ResultSet rs = sqlRun.SqlSelectStatement(sql);
+        String sql = "select distinct route from route_info"; //as data in route_info is de-normalised, we are selecting distinct route
+        ResultSet rs = sqlRun.SqlSelectStatement(sql); 
         routes = new ArrayList<>();
 
         while (rs.next()) {
@@ -24,7 +29,8 @@ public abstract class RouteMaster {
 
         System.out.println("\n" + "** Active Routes **");
         assert routes != null;
-
+        
+        //running a for each loop on the JDBC resultset to display and save route information
         for (String obj : routes) {
             sql = "select distinct stops from route_info where route='" + obj + "' ";
             rs = sqlRun.SqlSelectStatement(sql);
@@ -39,8 +45,14 @@ public abstract class RouteMaster {
         return routes;
     }
 
+    /*shared functionality of guest and user to select their home stop
+     * Approach: 
+     * 1. stops are divided in 4 directions east, west, north, south. Center being the employee's office.
+     * 2. Users/guests will not be given the feature to suggest new routes, instead they can request for a stop and Admin takes an informed decision on route modification.
+     * --
+     * selectstop method is an interactive method which checks which direction the user wants a stop and gives right options to select from
+     */
     boolean selectStop(String login) throws SQLException {
-        Connection con = JdbcConnect.connect();
         Scanner input = new Scanner(System.in);
         ArrayList<String> stops = new ArrayList<>();
         ArrayList<String> directions = new ArrayList<>();
@@ -48,59 +60,66 @@ public abstract class RouteMaster {
         System.out.println("\n" + "Enter the direction where you are looking for stop:  EAST , WEST , NORTH , SOUTH");
         System.out.print("Input: ");
 
-        if (con != null) {
             String sqlQuery = "select distinct direction from stop_info;";
-            PreparedStatement pstSel = con.prepareStatement(sqlQuery);
-            ResultSet rs1 = pstSel.executeQuery();
+            ResultSet rs1 = sqlRun.SqlSelectStatement(sqlQuery);
 
             while (rs1.next()) {
-                directions.add(rs1.getString("direction"));
+                directions.add(rs1.getString("direction")); 
             }
+            rs1.close();
             String direction;
 
             do {
-                direction = input.next().toUpperCase();
+                direction = input.next().toUpperCase(); //takes user input and validates
                 if (!directions.contains(direction))
                 {
                     System.out.print("Invalid Input. Please select Valid Direction: ");
                 }
             } while (!directions.contains(direction));
-
+            
+            //query to get stops from direction which user has selected
             String sqlQuery2 = "select stop, direction from stop_info where direction ='"+direction+"'";
             System.out.println("\nDirection - " + direction +"\nStop Name");
-            try{
-                PreparedStatement pstSel2 = con.prepareStatement(sqlQuery2);
-                ResultSet rs2    = pstSel2.executeQuery();
 
-                while(rs2.next()) {
+                ResultSet rs2    = sqlRun.SqlSelectStatement(sqlQuery2);
+                
+                while(rs2.next()) {		//prints entire resulset of the stops
                     stops.add(rs2.getString("stop"));
                     System.out.print("- -" + rs2.getString("stop"));
                 }
-            }catch (SQLException e) { System.out.println(e.getMessage());}
+                rs2.close();
 
             System.out.print("\nEnter Stop Name: ");
             String stopname;
 
             do {
-                stopname = input.next().toUpperCase();
+                stopname = input.next().toUpperCase(); //takes user input and validates
                 if (!stops.contains(stopname))
                 {
                     System.out.println("Invalid Input. Please select Valid stop name: ");
                 }
             } while (!stops.contains(stopname));
+            
+            //updates user's stop name in user_info table and changes his status to Pending for Admin to review the new request
+//            sql = "UPDATE user_info SET stop ='"+stopname+"', status='PENDING' WHERE login ='"+login+"' ";
+            
+            SQLUpdate su = new SQLUpdate();
+            HashMap<String, String> colValues = new HashMap<>();
+            HashMap<String, String> where = new HashMap<>();
 
-            String sql = "UPDATE user_info SET stop ='"+stopname+"', status='PENDING' WHERE login ='"+login+"' and '"+stopname+"' in (SELECT stop from stop_info where direction= '"+direction+"')";
-            try (
-                    PreparedStatement pstUpdt = con.prepareStatement(sql)) {
-                pstUpdt.executeUpdate();
-            } catch (SQLException e) {
-                System.out.println(e.getMessage());
-            }
-            System.out.println("Stop Request sent for :" + login + "\nApproval : PENDING");
-            JdbcConnect.closeCon(con);
-            return true;
-        }
-        return false;
+            boolean isUploaded = false;
+                               
+                colValues.clear();
+                String tableName = "user_info";
+                colValues.put("stop", "'"+stopname+"'");
+                colValues.put("status", "'" + "PENDING" + "'");
+                where.put("login", "'" + login + "'");
+
+                isUploaded = su.ExecuteUpdate(tableName, colValues, where);
+                if(isUploaded) System.out.println("Stop Request sent for :" + login + "\nApproval : PENDING");
+                else System.out.println("Request Failed");
+            
+            return isUploaded;
     }
 }
 
